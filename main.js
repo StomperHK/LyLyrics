6022f253fdce70aebcad479f4483e635586a9f2f
Rafael Ribeiro <cortadordeunha666@gmail.com>
2022-05-05 22:37:38 -0300

    feat: prepare initial structure for requests.

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-const URL = 'https://api.lyrics.ovh/suggest/hero'
+const URL = 'https://api.lyrics.ovh/suggest/'
+
+const mainFormEL = document.querySelector('[data-js="main-form"]')
+const searchInputEL = document.querySelector('[data-js="search-input"]')
 
 
-function fetchSongs() {
-  fetch(URL)
+function getSearchTerm() {
+  return searchInputEL.value.trim()
+}
+
+function fetchSongs(event) {
+  event.preventDefault()
+
+  const searchTerm = getSearchTerm()
+  const searchTermIsEmpty = searchTerm.replaceAll(' ', '').length === 0
+  
+  if (searchTermIsEmpty) return
+
+  fetch(URL + searchTerm)
   .then(response => response.json())
   .then(data => console.log(data))
 }
+
+
+mainFormEL.addEventListener('submit', fetchSongs)
